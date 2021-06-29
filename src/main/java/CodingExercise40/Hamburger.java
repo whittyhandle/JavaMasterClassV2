49f570b2680b5efb4eeecd2755d4dc91ff69db9c
whittyhandle <kittledavid@gmail.com>
2021-06-28 21:16:54 -0500

    added 5 methods

--- src/main/java/CodingExercise40/Hamburger.java
+++ src/main/java/CodingExercise40/Hamburger.java
@@ -23,5 +23,30 @@ public class Hamburger {
         this.beadRollType = beadRollType;
     }
 
+    public void addHamburgerAddition1(String addition1Name, double addition1Price) {
+        this.addition1Name = addition1Name;
+        this.addition1Price = addition1Price;
+    }
+    public void addHamburgerAddition2(String addition2Name, double addition2Price) {
+        this.addition2Name = addition2Name;
+        this.addition2Price = addition2Price;
+    }
+    public void addHamburgerAddition3(String addition3Name, double addition3Price) {
+        this.addition3Name = addition3Name;
+        this.addition3Price = addition3Price;
+    }
+    public void addHamburgerAddition4(String addition4Name, double addition4Price) {
+        this.addition4Name = addition4Name;
+        this.addition4Price = addition4Price;
+    }
+
+    public double itemizehamburger(){
+        return this.price +
+                this.addition1Price +
+                this.addition2Price +
+                this.addition3Price +
+                this.addition4Price;
+    }
+
 
 }
