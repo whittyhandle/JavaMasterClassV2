3e5160e59c17a1565d2adb7bfe1e9fe264ceebf0
whittyhandle <kittledavid@gmail.com>
2021-06-28 21:05:41 -0500

    variables and constructor

--- src/main/java/CodingExercise40/Hamburger.java
+++ src/main/java/CodingExercise40/Hamburger.java
@@ -1,4 +1,27 @@
 package CodingExercise40;
 
 public class Hamburger {
+
+    String name;
+    String meat;
+    double price;
+    String beadRollType;
+    String addition1Name;
+    String addition2Name;
+    String addition3Name;
+    String addition4Name;
+    double addition1Price;
+    double addition2Price;
+    double addition3Price;
+    double addition4Price;
+
+
+    public Hamburger(String name, String meat, double price, String beadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.beadRollType = beadRollType;
+    }
+
+
 }
